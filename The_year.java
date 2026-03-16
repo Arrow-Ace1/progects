0a6b8da3da856f9dc65a8e6cecb3a37774bcaed1
Arrow-Ace1 <murphdre000@rocori.k12.mn.us>
2026-03-16 16:44:21 +0000

    ty

---
+++ The_year.java
@@ -0,0 +1,9 @@
+public class The_year {
+    
+    public static void main(String[] args){
+        int year = 2026;
+        System.out.println("welcome to java");
+        System.out.println("the year is.   " + year);
+    }
+    
+}
